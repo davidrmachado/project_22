77fb57f432777b0c45131c457084748c393889ba
David-Neto <davidrmachado@gmail.com>
2022-12-29 17:57:31 -0300

    Entrega do requisito 10

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,5 @@
+// 3 - Retorne o lanche mais vendido, mostrando apenas o nome e a quantidade do lanche mais vendido:
+
 db.produtos
   .find({ vendidos: { $gt: 50, $lt: 100 } }, { _id: 0, nome: 1, vendidos: 1 })
   .sort({ vendidos: 1 });
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,2 +1,5 @@
+// 7 - Retorne o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente
+// de 50 e em que o campo tags não exista:
+
 db.produtos.find({ curtidas: { $ne: 50 }, tags: { $exists: false } },
     { _id: 0, nome: 1, vendidos: 1 });
