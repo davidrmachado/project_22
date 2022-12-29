75969b8f2a853f1106ac649fa4f5ea021596c95d
David-Neto <davidrmachado@gmail.com>
2022-12-29 18:49:46 -0300

    Entrega do requisito 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,7 @@
+// 17 - Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos:
+
+db.resumoProdutos.insert(
+    { franquia: "McDonalds", totalProdutos: db.produtos.countDocuments({}) },
+);
+
+db.resumoProdutos.find({}, { _id: 0, franquia: 1, totalProdutos: 1 });
